fccf66ff23a66494f2fc456130f3b39b41a75d5e
Krzysztof Pleskot <krzypl@amu.edu.pl>
2025-02-26 22:52:51 +0100

    next commit

--- 12_graphical_abstract.R
+++ 12_graphical_abstract.R
@@ -9,30 +9,30 @@ rap_vs_meas_dry_ws_l <- read_csv("data/rap_vs_meas_dry_ws_l.csv")
 rap_vs_meas_wet_ws_l_ratio <- read_csv("data/rap_vs_meas_wet_ws_l_ratio.csv")
 
 plot_a <- rap_vs_meas_dry_ws_l %>% 
-  filter(element == "K") %>% 
+  filter(element == "Fe") %>% 
   ggplot(aes(x = standard_conc, y = measured_conc)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, alpha = 0.4) +
   geom_abline(intercept = 0, slope = 1, color = "black", alpha = 0.3) +
   theme(legend.position = "bottom") +
-  ggtitle(label = "K") +
+  ggtitle(label = "Fe") +
   labs(x = "Certified concentration (ppm)", y = "Averaged measured concentration (ppm)") +
   theme(plot.title = element_text(hjust = 0.5))
 
 plot_b <- rap_vs_meas_wet_ws_l_ratio %>% 
-  filter(ratio_id_log == "log(K/Ca)") %>% 
+  filter(ratio_id_log == "log(Fe/Ca)") %>% 
   ggplot(aes(x = standard_ratio_log, y = measured_ratio_log)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, alpha = 0.4) +
   geom_abline(intercept = 0, slope = 1, color = "black", alpha = 0.3) +
   theme(legend.position = "bottom") +
-  ggtitle("log(K/Ca)") +
+  ggtitle("log(Fe/Ca)") +
   labs(x = "Certified elemental log ratio", y = "Measured elemental log ratio") +
   theme(plot.title = element_text(hjust = 0.5))
 
 combined_ab <- grid.arrange(plot_a, 
                             plot_b,
-                            ncol = 1)
+                            ncol = 2)
 
 ggsave(filename = "figures/fig_plots_for_ga.svg",
        plot = combined_ab,
